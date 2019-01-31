8f831c3afc5137f0e9657b715ef2258d8fa0f8f4
Ashwin Madavan <ashwin.madavan@gmail.com>
2019-01-30 19:17:17 -0500

    UpdateE

--- src/main/java/com/ashwin/fri/stocks/ForecastTester.java
+++ src/main/java/com/ashwin/fri/stocks/ForecastTester.java
@@ -1,6 +1,9 @@
 package com.ashwin.fri.stocks;
 
-import com.ashwin.fri.stocks.hibernate.Registrant;
+import com.ashwin.fri.stocks.hibernate.HibernateConfig;
+import com.ashwin.fri.stocks.hibernate.Submission;
+
+import org.hibernate.Session;
 
 public class ForecastTester {
 	
@@ -10,19 +13,20 @@ public class ForecastTester {
 //		Date end   = new Date(System.currentTimeMillis());
 //		Set<Submission> training = forecast.train(start, end, 0.90, 1.0);
 //		System.out.println("Successfully trained on " + training.size() + " submissions");
-//		
-//		Submission submission = new Submission();
-//		submission.setAdsh("0000311471-14-000006");
-//		submission.setFilerStatus(FilerStatus.ACCELERATED);
+
+		Session session = HibernateConfig.FACTORY.openSession();
+		Submission submission = new Submission();
+		submission.setAdsh("0000311471-14-000006");
+		submission.setFilerStatus(Submission.FilerStatus.ACCELERATED);
 //		System.out.println(forecast.predict(submission));
-//		submission.setFilerStatus(FilerStatus.LARGE_ACCELERATED);
+//		submission.setFilerStatus(Submission.FilerStatus.LARGE_ACCELERATED);
 //		System.out.println(forecast.predict(submission));
 		
-		Registrant apco = new Registrant();
-		apco.setCik(311471);
-		apco.setSic(1311);
-		
-		DCFAnalysis dcf = new DCFAnalysis(1311);
-		System.out.println(dcf.value(apco, 38800000, 32480000, 3370000, 5817000, 72040000, 38020000));
+//		Registrant apco = new Registrant();
+//		apco.setCik(311471);
+//		apco.setSic(1311);
+//
+//		DCFAnalysis dcf = new DCFAnalysis(1311);
+//		System.out.println(dcf.value(apco, 38800000, 32480000, 3370000, 5817000, 72040000, 38020000));
 	}
 }
